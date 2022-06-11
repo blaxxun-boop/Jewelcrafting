8fed85a008c24e6bf32c059cefb3b524e27e53c4
blaxxun <blaxxun@blaxxun.blaxxun>
2022-06-12 00:09:50 +0200

    bump version

--- Jewelcrafting/Jewelcrafting.cs
+++ Jewelcrafting/Jewelcrafting.cs
@@ -22,7 +22,7 @@ namespace Jewelcrafting;
 public partial class Jewelcrafting : BaseUnityPlugin
 {
 	public const string ModName = "Jewelcrafting";
-	private const string ModVersion = "1.0.0";
+	private const string ModVersion = "1.0.1";
 	private const string ModGUID = "org.bepinex.plugins.jewelcrafting";
 
 	public static readonly ConfigSync configSync = new(ModName) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
