d4e06402310a82e7c0aedfc7f238d903cc841b11
blaxxun <blaxxun@blaxxun.blaxxun>
2023-08-03 19:36:58 +0200

    release v1.4.15

--- Jewelcrafting/Jewelcrafting.cs
+++ Jewelcrafting/Jewelcrafting.cs
@@ -27,7 +27,7 @@ namespace Jewelcrafting;
 public partial class Jewelcrafting : BaseUnityPlugin
 {
 	public const string ModName = "Jewelcrafting";
-	private const string ModVersion = "1.4.14";
+	private const string ModVersion = "1.4.15";
 	private const string ModGUID = "org.bepinex.plugins.jewelcrafting";
 
 	public static readonly ConfigSync configSync = new(ModName) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
--- Jewelcrafting/WorldBosses/GemStoneInteract.cs
+++ Jewelcrafting/WorldBosses/GemStoneInteract.cs
@@ -64,7 +64,7 @@ public class GemStoneInteract : MonoBehaviour, Interactable, Hoverable
 			DateTime endDate = prizes.EndDate;
 			if (endDate == DateTime.MaxValue && GachaChest.Expiration(prizes) != DateTimeOffset.MinValue)
 			{
-				endDate = GachaChest.Expiration(prizes).Date;
+				endDate = GachaChest.Expiration(prizes).UtcDateTime;
 			}
 			window.transform.Find("Bkg/Middle_Season_Banner/Season_Time").GetComponent<Text>().text = endDate != DateTime.MaxValue ? Localization.instance.Localize("$jc_gacha_time_left", Utils.GetHumanFriendlyTime((int)(endDate - DateTime.UtcNow).TotalSeconds)) : "";
 			window.transform.Find("Bkg/Coins_Middle/Middle_Coins_Text").GetComponent<Text>().text = Localization.instance.Localize("$jc_gacha_coins: ") + Player.m_localPlayer.m_inventory.CountItems(gachaCoinName);
