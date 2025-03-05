1a78f46c1e17be3c1350230f110f23c5af8fc07b
blaxxun <blaxxun@blaxxun.blaxxun>
2025-03-04 20:20:36 -0500

    release v1.5.33

--- Jewelcrafting/Jewelcrafting.cs
+++ Jewelcrafting/Jewelcrafting.cs
@@ -31,7 +31,7 @@ namespace Jewelcrafting;
 public partial class Jewelcrafting : BaseUnityPlugin
 {
 	public const string ModName = "Jewelcrafting";
-	private const string ModVersion = "1.5.32";
+	private const string ModVersion = "1.5.33";
 	private const string ModGUID = "org.bepinex.plugins.jewelcrafting";
 
 	public static readonly ConfigSync configSync = new(ModName) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
